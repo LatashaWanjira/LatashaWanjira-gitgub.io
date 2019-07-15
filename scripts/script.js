a21b3ae663549b57ad5b28937705f270250c5295
LatashaWanjira <latasha.wanjira@gmail.com>
2019-07-15 19:28:27 +0300

    changed links for the sites and fixed the navbar

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,28 @@
 $(document).ready(function() {
+
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function() {
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+
   // Toggle the sideNavMenu
   $('#menuToggle').on('click', function() {
     $('#sideNavMenu').toggleClass("show hide");
@@ -8,7 +32,15 @@ $(document).ready(function() {
     $('#sideNavMenu').addClass("hide");
     $('#sideNavMenu').removeClass("show");
     $('#menuToggle i').toggleClass("fa-times fa-bars");
-
+  });
+  $(window).click(function() {
+    if ($("#menuToggle i").hasClass('fa-times')) {
+      $("#menuToggle i").toggleClass('fa-times fa-bars');
+      $("#sideNavMenu").toggleClass('show hide');
+    }
+  });
+  $('#menuToggle').click(function(event) {
+    event.stopPropagation();
   });
 
   // Adding active class to selected link
